9b766beccbc19391a69bca733d245ba0eada5a2a
Ruslan <40143122+airkross@users.noreply.github.com>
2019-01-03 04:50:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $(".skills").click(function () {
+//     $(".copyright").hide();
+//   });
+// });
+
+// var modalContent = document.getElementsByClassName(".modal-content");
+// var header = document.getElementsByClassName(".header");
+// console.log(modalContent);
+// if(!modalContent.click()){
+//   header.style.display = "block";
+// }
